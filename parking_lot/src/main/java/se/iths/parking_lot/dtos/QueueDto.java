b719220b99c97695a16cf6841f4a3d8eb7c71af8
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2022-04-26 13:51:29 +0200

    Queue & QueueSlot - Using mapper to convert endity to DTO

--- parking_lot/src/main/java/se/iths/parking_lot/dtos/QueueDto.java
+++ parking_lot/src/main/java/se/iths/parking_lot/dtos/QueueDto.java
@@ -1,21 +1,10 @@
 package se.iths.parking_lot.dtos;
 
-import se.iths.parking_lot.entities.Queue;
-import se.iths.parking_lot.entities.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.List;
 
-public record QueueDto(Long id, List<QueueSlotDto> queueSlots) {
-    public static QueueDto from(Queue queue) {
-        return new QueueDto(queue.getId(),QueueSlotDto.from(queue.getQueueSlots()));
-    }
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record QueueDto( Long id, List<QueueSlotDto> queueSlots) {
 
-    public static List<QueueDto> from(List<Queue> queues) {
-        return queues.stream()
-                .map(queue -> QueueDto.from(queue)).toList();
-    }
-
-    public Queue toQueue() {
-        return Queue.fromDto(this);
-    }
 }
--- parking_lot/src/main/java/se/iths/parking_lot/dtos/QueueSlotDto.java
+++ parking_lot/src/main/java/se/iths/parking_lot/dtos/QueueSlotDto.java
@@ -1,18 +1,8 @@
 package se.iths.parking_lot.dtos;
 
-import se.iths.parking_lot.entities.ParkingSlot;
-import se.iths.parking_lot.entities.QueueSlot;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.List;
-
-public record QueueSlotDto(Long id, Boolean electricCharge) {
-
-    public static QueueSlotDto from(QueueSlot queueSlot) {
-        return new QueueSlotDto(queueSlot.getId(), queueSlot.getElectricCharger());
-    }
-    public static List<QueueSlotDto> from(List<QueueSlot> queueSlots) {
-        return queueSlots.stream()
-                .map(QueueSlotDto::from).toList();
-    }
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record QueueSlotDto(Long id, Boolean electricCharge, UserDto user) {
 
 }
