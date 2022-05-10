eded3c232d08e29c86fa3e0da4c30c8a5057f6bd
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2022-05-10 10:41:25 +0200

    Added css for login

--- parking_lot/src/main/java/se/iths/parking_lot/config/SecurityConfigurer.java
+++ parking_lot/src/main/java/se/iths/parking_lot/config/SecurityConfigurer.java
@@ -1,7 +1,9 @@
 package se.iths.parking_lot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import se.iths.parking_lot.model.SuccessHandler;
 import se.iths.parking_lot.services.MyUserDetailsService;
 
 @Configuration
@@ -22,11 +25,13 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final MyUserDetailsService myUserDetailsService;
     private final JwtRequestFilter jwtRequestFilter;
+    private final SuccessHandler successHandler;
 
-    public SecurityConfigurer(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, MyUserDetailsService myUserDetailsService, JwtRequestFilter jwtRequestFilter) {
+    public SecurityConfigurer(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, MyUserDetailsService myUserDetailsService, JwtRequestFilter jwtRequestFilter, SuccessHandler successHandler) {
         this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
         this.myUserDetailsService = myUserDetailsService;
         this.jwtRequestFilter = jwtRequestFilter;
+        this.successHandler = successHandler;
     }
 
 
@@ -38,11 +43,15 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/authenticate", "/login", "/tl_users/create", "/css/**")
+                .antMatchers("/authenticate", "/login", "/tl_users/create", "/css/**","/static/**")
                 .permitAll()
-                .antMatchers("/tl_parking_lots").hasAnyRole("MANAGER", "ADMIN")
-                .antMatchers("/tl_parking_lots/add").hasRole("ADMIN")
+                .antMatchers("/tl_parking_lots/add", "/tl_parking_lots/delete/**").hasRole("ADMIN")
+                .antMatchers("/tl_parking_lots", "/tl_parking_lots/").hasAnyRole("MANAGER", "ADMIN")
                 .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .successHandler(successHandler)
                 .and()
                 .logout()
                 .logoutUrl("/logout")
@@ -67,6 +76,15 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("{noop}pass").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}pass").roles("ADMIN");
     }
 
 }
